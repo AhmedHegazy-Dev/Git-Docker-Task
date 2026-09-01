26163acbfa3adb23175085d8e30be9b1d777402c
Ahmed hegazy <121024792+hegzo891@users.noreply.github.com>
2026-09-01 15:54:39 +0300

    Add sales data validation

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,14 +1,16 @@
 import os
 import pandas as pd
 
-   
-    
+
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sales_file_path = os.path.join(base_dir, "data", "sales.csv")
     
 sales_df = pd.read_csv(sales_file_path)
 print("Extracted Data:")
 print(sales_df)
+
+sales_df = sales_df[(sales_df['quantity'] > 0) & (sales_df['price'] >= 0)]
+
 total = (sales_df['quantity'] * sales_df['price']).sum()
 
 save_file_path = os.path.join(base_dir, "data", "processed_sales.txt")
