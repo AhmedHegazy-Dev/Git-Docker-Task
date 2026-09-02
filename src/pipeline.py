4363bac4cf69e9cc95e1ddfd26985829d37f52f0
Ahmed hegazy <121024792+hegzo891@users.noreply.github.com>
2026-09-02 18:54:55 +0300

    add spaces

--- src/pipeline.py
+++ src/pipeline.py
@@ -20,7 +20,7 @@ total = (sales_df['quantity'] * sales_df['price']).sum()
 save_file_path = os.path.join(base_dir, "data", "processed_sales.txt")
 
 with open(save_file_path, "w") as f:
-    f.write(f"Total Sales: {total}")
-    f.write(f"Total Quantity: {quantity}")
-    f.write(f"Total products: {count}")
+    f.write(f"Total Sales: {total}\n")
+    f.write(f"Total Quantity: {quantity}\n")
+    f.write(f"Total products: {count}\n")
 
